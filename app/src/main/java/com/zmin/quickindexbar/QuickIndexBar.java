1124c778bdf84f9969768077beffc8c09da23cd6
zhangmin <zhangmin9x9@163.com>
2017-11-01 17:48:12 +0800

    更新

--- app/src/main/java/com/zmin/quickindexbar/QuickIndexBar.java
+++ app/src/main/java/com/zmin/quickindexbar/QuickIndexBar.java
@@ -111,10 +111,6 @@ public class QuickIndexBar extends View {
         }
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
